6f228d257ac089dc786cdc02d43c40d5e6bde10b
vinaya-sv <95991141+vinaya-sv@users.noreply.github.com>
2024-03-02 21:53:59 +0530

    02-03-2024

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import SignIn from './Components/sign_in';
